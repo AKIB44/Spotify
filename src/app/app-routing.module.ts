a12124a87fd8e68802db9756b2a10435a90e90d4
akiii02 <80379607+akiii02@users.noreply.github.com>
2022-05-31 11:06:03 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,76 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppsigninComponent } from './components/appsignin/appsignin.component';
+import { DownloadComponent } from './components/download/download.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomesectionComponent } from './components/home/homesection/homesection.component';
+import { PlaysongComponent } from './components/home/playsong/playsong.component';
+import { ViewallComponent } from './components/home/viewall/viewall.component';
+import { IndexComponent } from './components/index/index.component';
+import { Index2Component } from './components/index2/index2.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+const routes: Routes = [
+
+  // Default Path
+
+  {
+    path: '',
+    redirectTo : 'index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  },
+  {
+    path: 'login',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'index2',
+    component: Index2Component
+  },
+  {
+    path: 'download',
+    component: DownloadComponent
+  },
+  {
+    path: 'applogin',
+    component: AppsigninComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children:[
+      {
+        path: 'songs',
+        component: HomesectionComponent
+      },
+      {
+        path: 'view',
+        component: ViewallComponent
+      },
+      {
+        path: 'play',
+        component: PlaysongComponent
+      }
+    ]
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Section1Component } from './components/index/section1/section1.component';
+import { Section2Component } from './components/index/section2/section2.component';
+import { Section3Component } from './components/index/section3/section3.component';
+import { Section4Component } from './components/index/section4/section4.component';
+import { Section5Component } from './components/index/section5/section5.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { Index2Component } from './components/index2/index2.component';
+import { DownloadComponent } from './components/download/download.component';
+import { AppsigninComponent } from './components/appsignin/appsignin.component';
+import { HomesectionComponent } from './components/home/homesection/homesection.component';
+import { ViewallComponent } from './components/home/viewall/viewall.component';
+import { PlaysongComponent } from './components/home/playsong/playsong.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    SigninComponent,
+    SignupComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component,
+    Section5Component,
+    Index2Component,
+    DownloadComponent,
+    AppsigninComponent,
+    HomesectionComponent,
+    ViewallComponent,
+    PlaysongComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
